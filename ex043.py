5d0c389d0fc8e5e21f3031e0904e5402ee2c79fc
Leandro Tavares <leandro.mendestavares@gmail.com>
2019-12-28 09:22:30 -0300

    ex43

--- ex043.py
+++ ex043.py
@@ -9,4 +9,17 @@
 # vars
 peso = float(input('Qual é o seu peso: '))
 altura = float(input('Qual é a sua altura: '))
+imc = peso / (altura * altura)
 
+if imc < 18.5:
+    print('Seu IMC é: {:.2f}. Você está abaixo do peso!'.format(imc))
+elif 18.5 <= imc < 25:
+    print('Seu IMC é: {:.2f}. Você está no peso ideal!'.format(imc))
+elif 25 <= imc < 30:
+    print('Seu IMC é: {:.2f}. Você está com sobrepeso!'.format(imc))
+elif 30 <= imc <= 40:
+    print('Seu IMC é: {:.2f}. Você está obeso!'.format(imc))
+elif imc > 40:
+    print('Seu IMC é: {:.2f}. Você está com obesidade mórbida!'.format(imc))
+else:
+    print('Seu IMC é: {:.2f}. Valor não identificado!'.format(imc))
